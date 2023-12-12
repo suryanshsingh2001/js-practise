d19241fd474fea0880d5d4a37322084bbad46795
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2023-12-12 10:49:31 +0530

    Add

---
+++ index.js
@@ -0,0 +1,21 @@
+let arr = [
+    {name: 'John', age: 25},
+    {name: 'Pete', age: 30},
+    {name: 'Mary', age: 28}
+];
+
+// Map
+// let names = arr.map((item) => item.name + ' ' + item.age);
+// console.log(names); // John 25, Pete 30, Mary 28
+
+
+// .for-each
+arr.forEach((item) => item.age += 1);
+console.log(arr);
+
+// .filter
+let tooYoung = arr.filter((item) => item.age <= 30);
+console.log(tooYoung);
+
+// .reduce
+let totalAge = 
